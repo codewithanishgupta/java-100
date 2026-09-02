3dc2d4c7f1249644b99016be10fa7677135afeb1
codewithanishgupta <anishprabhakar84@gmail.com>
2026-09-02 08:35:17 +0530

    pattern Questions

---
+++ Pattern Programs/Q2.java
@@ -0,0 +1,15 @@
+// 2 Print a pyramid star pattern.
+
+public class Q2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
